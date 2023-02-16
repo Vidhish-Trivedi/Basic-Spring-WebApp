c6c13b350c555f36acc4ecedaeb32b82b3cc5533
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-02-17 00:49:36 +0530

    Updated to work with frontend

--- src/main/java/com/vtmovies/movies/ReviewController.java
+++ src/main/java/com/vtmovies/movies/ReviewController.java
@@ -1,6 +1,7 @@
 package com.vtmovies.movies;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
